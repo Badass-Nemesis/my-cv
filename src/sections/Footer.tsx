df7a8f45cc0e77cfb4cfcaf0b6df8b1881836fb1
Badass-Nemesis <harshitanant29@gmail.com>
2024-11-24 18:29:48 +0530

    fixed footer font

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="fixed bottom-0 w-full flex justify-center items-center bg-[#181819] border-t border-gray-300 text-gray-300">
+        <footer className="fixed bottom-0 w-full flex justify-center items-center bg-[#181819] border-t border-gray-300 font-content text-sm text-gray-300">
             This is footer
         </footer>
     );
